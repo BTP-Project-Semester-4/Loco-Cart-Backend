feb1584fe3620b1be4ae186d881c5db3a803ecd8
NALIN AGRAWAL <54065357+nalin-programmer@users.noreply.github.com>
2020-12-18 22:31:12 +0530

    Created Seller model

--- model/Seller.js
+++ model/Seller.js
@@ -1,1 +1,22 @@
+const mongoose = require("mongoose");
+
+const sellerSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  contactNo: { type: String, required: true },
+  rating: { type: Number, default: 0, required: true },
+  category: { type: String, required: true },
+  homeDelivery: { type: Boolean, default: false, required: true },
+  deliveryCharges: { type: Number, default: 0, required: false },
+  location: { type: String, required: true },
+  password: { type: String, default: false, required: true },
+  profilePictureUrl: {
+    type: String,
+    default:
+      "https://i0.wp.com/www.repol.copl.ulaval.ca/wp-content/uploads/2019/01/default-user-icon.jpg",
+    required: true,
+  },
+});
+const Order = mongoose.model("Seller", sellerSchema);
+export default Seller;
 
