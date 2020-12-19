b7a7bf2b8bc53ad3cd0288fc9fe8486669c72524
vijayjoshi16 <joshivijay324@gmail.com>
2020-12-19 18:25:20 +0530

    Add Order Model

---
+++ model/Order.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema(
+  {
+     customerId: {
+       type: mongoose.Schema.Types.ObjectId,
+       ref: "Customer",
+       required:true
+     },
+     seller:{
+       type: [
+          {
+           type: mongoose.Schema.Types.ObjectId,
+           ref:"Seller"
+          }
+       ],
+       default:[]
+     },
+     itemList:{
+       type: [
+          {
+            productId: {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "Product"
+            },
+            quantity:{
+              type: Number
+            }
+          }
+       ],
+       required: true
+     },
+     isBid:{
+       type: Boolean,
+       default: false
+     },
+     totalPrice:{
+       type: Number,
+       required: true
+     }
+  }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
