1e1aa78d84ed279d8e705ffa59762b38ad407dad
NALIN AGRAWAL <54065357+nalin-programmer@users.noreply.github.com>
2020-12-21 15:49:31 +0530

    seller sign in and sign up routes created

--- model/Seller.js
+++ model/Seller.js
@@ -17,6 +17,4 @@ const sellerSchema = new mongoose.Schema({
     required: true,
   },
 });
-const Seller = mongoose.model("Seller", sellerSchema);
-export default Seller;
-
+module.exports = mongoose.model("Seller", sellerSchema);
