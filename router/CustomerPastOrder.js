dfa40b6844af702df530a64451a5d6785dcfa0ca
Prerit Kumar Jha <preritkrjha@gmail.com>
2020-12-27 23:18:43 +0530

    Customer Past Order Route Created

---
+++ router/CustomerPastOrder.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const Order = require("../model/Order.js");
+const expressAsyncHandler = require("express-async-handler");
+const env = require('dotenv');
+const customerPastOrderRouter = express.Router();
+
+customerPastOrderRouter.get(
+    "/customer_past_order", (req,res) => {
+    Order.find({ customerId : req.body.customerId })
+        .exec((err, history) => {
+          if (err) {
+            return res.status(422).json({ error: err });
+          }
+          return res.json({ history });
+    })
+});
+
+module.exports = customerPastOrderRouter;
