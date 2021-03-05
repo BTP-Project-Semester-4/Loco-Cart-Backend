c4be5fd33836b34a223767ca03709388c2b4ef57
Prerit Kumar Jha <preritkrjha@gmail.com>
2021-03-05 19:57:36 +0530

    Past Order seller

--- router/SellerPastOrder.js
+++ router/SellerPastOrder.js
@@ -4,14 +4,15 @@ const expressAsyncHandler = require("express-async-handler");
 const env = require('dotenv');
 const sellerPastOrderRouter = express.Router();
 
-sellerPastOrderRouter.get(
+sellerPastOrderRouter.post(
     "/seller_past_order", (req,res) => {
     Order.find({ sellerId : req.body.sellerId })
         .exec((err, history) => {
           if (err) {
             return res.status(422).json({ error: err });
           }
-          return res.json({ history });
+          else
+            return res.json({ history });
     })
 });
 
